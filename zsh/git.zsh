78782755d70be54c4a7613936d1eb7f0edfb78c5
Dantong Jin <j.dantong@gmail.com>
2026-02-24 23:42:55 +0800

    chore(git): rename scripts and update aliases

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,3 +1,3 @@
-git config --global alias.ck '!$HOME/WorkSpace/dotfiles/scripts/git-checkout-by-pattern.sh'
+git config --global alias.ck '!$HOME/WorkSpace/dotfiles/scripts/git-checkout-by-keyword.sh'
 git config --global alias.pm '!$HOME/WorkSpace/dotfiles/scripts/git-push-merge.sh'
-git config --global alias.gcm '!$HOME/WorkSpace/dotfiles/scripts/generate-commit-message.sh'
+git config --global alias.gcm '!$HOME/WorkSpace/dotfiles/scripts/git-generate-commit-message.sh'
