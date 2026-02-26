5ae90bd7521d1b88c55ba1bee7b9270eeb9b52a4
Dantong Jin <j.dantong@gmail.com>
2026-02-26 23:22:51 +0800

    feat: configure telescope to show hidden files

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -36,6 +36,17 @@ return {
             },
           },
         },
+        pickers = {
+          find_files = {
+            hidden = true,
+            no_ignore = true,
+          },
+          live_grep = {
+            additional_args = function()
+              return { "--hidden", "--no-ignore", "--glob", "!.git/*" }
+            end,
+          },
+        },
       }
     end,
   },
