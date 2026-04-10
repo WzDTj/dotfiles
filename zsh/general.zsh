faca593abda5077e9495d637662ed8ba3ac43e38
Dantong Jin <j.dantong@gmail.com>
2026-04-10 11:33:33 +0800

    fix(zsh): use uppercase proxy vars

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -16,9 +16,9 @@ alias fm=$FILE_MANAGER
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 
 # Proxy by surge
-export https_proxy=http://127.0.0.1:6152
-export http_proxy=http://127.0.0.1:6152
-export all_proxy=socks5://127.0.0.1:6153
+export HTTPS_PROXY=http://127.0.0.1:6152
+export HTTP_PROXY=http://127.0.0.1:6152
+export ALL_PROXY=socks5://127.0.0.1:6153
 export NO_PROXY=localhost,127.0.0.1
 
 # Maestro
